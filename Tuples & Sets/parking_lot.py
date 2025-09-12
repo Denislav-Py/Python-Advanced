58ceb64e68df35e93beb79a904bcb76abd5d8eab
Denislav Iliev <dbiliev@gmail.com>
2025-09-12 19:19:39 +0200

    Create parking_lot.py

---
+++ Tuples & Sets/parking_lot.py
@@ -0,0 +1,25 @@
+# Write a program that:
+# •	Records a car number for every car that enters the parking lot
+# •	Removes a car number when the car leaves the parking lot
+# On the first line, you will receive the number of commands - N.
+# On the following N lines, you will receive the direction and car's number in the format: "{direction}, {car_number}".
+# The direction could only be "IN" or "OUT". Print the car numbers which are still in the parking lot.
+# Keep in mind that all car numbers must be unique. If the parking lot is empty, print "Parking Lot is Empty".
+# Note: The order in which we print the result does not matter.
+
+n = int(input())
+
+car_lot = set()
+
+for _ in range(n):
+    direction, plate = input().split(", ")
+    if direction == "IN":
+        car_lot.add(plate)
+    elif direction == "OUT":
+        car_lot.remove(plate)
+
+if not car_lot:
+    print("Parking Lot is Empty")
+else:
+    for plate in car_lot:
+        print(plate)
